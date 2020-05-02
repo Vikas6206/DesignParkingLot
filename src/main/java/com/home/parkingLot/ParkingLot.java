3c4dbb8e4f489e4e965cea2ac4da51a48f8540f8
Vikas6206 <vikas.kr6206@gmail.com>
2020-05-02 16:02:52 +0530

    Modified parking lot to set the data

--- src/main/java/com/home/parkingLot/ParkingLot.java
+++ src/main/java/com/home/parkingLot/ParkingLot.java
@@ -6,6 +6,11 @@ public class ParkingLot {
     private List<Level> levelList;
     private List<Booking> bookingList;
 
+    public ParkingLot(List<Level> levelList, List<Booking> bookingList) {
+        this.levelList = levelList;
+        this.bookingList = bookingList;
+    }
+
     public List<Level> getLevelList() {
         return levelList;
     }
