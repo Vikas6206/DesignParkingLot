57f6d412ff3f258d28ea2cdd3e2220e2d30ec6d1
Vikas6206 <vikas.kr6206@gmail.com>
2020-05-02 13:49:37 +0530

    Updated the Row class

--- src/main/java/com/home/parkingLot/Row.java
+++ src/main/java/com/home/parkingLot/Row.java
@@ -1,4 +1,29 @@
 package com.home.parkingLot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Row {
+    private int RowNo;
+    private int LevelNo;
+    private List<Slot> slotList;
+
+    public Row(int rowNo, int levelNo) {
+        //check if levelNo is null to throw illegal argument exception
+        RowNo = rowNo;
+        LevelNo = levelNo;
+        this.slotList=new ArrayList<>();
+    }
+
+    public int getRowNo() {
+        return RowNo;
+    }
+
+    public int getLevelNo() {
+        return LevelNo;
+    }
+
+    public List<Slot> getSlotList() {
+        return slotList;
+    }
 }
