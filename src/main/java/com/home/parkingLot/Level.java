4ba2ad69b2f959a54fb3d037298c97280c8e2395
Vikas6206 <vikas.kr6206@gmail.com>
2020-05-02 13:44:38 +0530

    Updated the level class

--- src/main/java/com/home/parkingLot/Level.java
+++ src/main/java/com/home/parkingLot/Level.java
@@ -1,4 +1,32 @@
 package com.home.parkingLot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Level {
+    private int LevelNo;
+    private ParkingLot parkingLot;
+    private List<Row> rowList;
+
+    public Level(int levelNo, ParkingLot parkingLot) {
+
+        if(parkingLot == null){
+            throw new IllegalArgumentException("Parking not is null");
+        }
+        LevelNo = levelNo;
+        this.parkingLot = parkingLot;
+        rowList=new ArrayList<>();
+    }
+
+    public int getLevelNo() {
+        return LevelNo;
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public List<Row> getRowList() {
+        return rowList;
+    }
 }
