26cf0863f8b26748765c6ca5dec67011e80ea2c9
Vikas6206 <vikas.kr6206@gmail.com>
2020-05-02 13:54:19 +0530

    Updated the slot class

--- src/main/java/com/home/parkingLot/Slot.java
+++ src/main/java/com/home/parkingLot/Slot.java
@@ -3,4 +3,23 @@ package com.home.parkingLot;
 public class Slot {
     private int rowNo;
     private int slotNo;
+    private Boolean isOccupied;
+
+    public Slot(int rowNo, int slotNo) {
+        this.rowNo = rowNo;
+        this.slotNo = slotNo;
+        this.isOccupied = Boolean.FALSE;
+    }
+
+    public int getRowNo() {
+        return rowNo;
+    }
+
+    public int getSlotNo() {
+        return slotNo;
+    }
+
+    public Boolean getOccupied() {
+        return isOccupied;
+    }
 }
