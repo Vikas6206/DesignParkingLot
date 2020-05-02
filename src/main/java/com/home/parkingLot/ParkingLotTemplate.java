915925f43e75fb80406bfe7b6774f6eb09623488
Vikas6206 <vikas.kr6206@gmail.com>
2020-05-02 15:14:25 +0530

    Added the parking lot template using template design pattern

--- src/main/java/com/home/parkingLot/ParkingLotTemplate.java
+++ src/main/java/com/home/parkingLot/ParkingLotTemplate.java
@@ -1,4 +1,46 @@
 package com.home.parkingLot;
 
-public class ParkingLotTemplate {
+import java.util.List;
+
+public abstract class ParkingLotTemplate {
+    protected ParkingLot parkingLot;
+
+    protected ParkingLotTemplate(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    protected abstract Vehicle registerVehicle(Vehicle vehicle);
+
+    protected abstract Row isParkingAvailable(VehicleSize vehicleSize, int noOfSlotNeeded);
+
+    protected abstract Booking book(Vehicle vehicle, Row row);
+
+    protected abstract Booking searchBooking(Vehicle vehicle);
+
+    protected abstract Payment pay(Booking booking);
+
+    protected abstract PaymentStatus clearSlot(List<Slot> slotList);
+
+    public IBooking booking(Vehicle vehicle) {
+        vehicle = registerVehicle(vehicle);
+        Row row = isParkingAvailable(vehicle.getVehicleSize(), vehicle.getSlotNeeded());
+
+        if (row == null) {
+            System.out.println("Parking not available!");
+            return new EmptyBooking();
+        }
+
+        Booking booking = book(vehicle, row);
+        return booking;
+
+    }
+
+    public PaymentStatus exit(Vehicle vehicle) {
+        Booking booking = searchBooking(vehicle);
+        Payment payment = pay(booking);
+        Booking book = (Booking) booking;
+        clearSlot(book.getSlotList());
+        return payment.getPaymentStatus();
+    }
+
 }
