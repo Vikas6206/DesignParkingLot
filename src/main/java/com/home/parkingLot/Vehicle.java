932bfa4808e61b6dfa095a856ef504e7907bc2dd
Vikas6206 <vikas.kr6206@gmail.com>
2020-05-02 14:03:18 +0530

    Updated the vehicle class

--- src/main/java/com/home/parkingLot/Vehicle.java
+++ src/main/java/com/home/parkingLot/Vehicle.java
@@ -1,4 +1,45 @@
 package com.home.parkingLot;
 
+import java.awt.*;
+
 public class Vehicle {
+    private VehicleSize vehicleSize;
+    private int numberPlate;
+    private Color color;
+    private String model;
+    private int slotNeeded;
+
+    public Vehicle(VehicleSize vehicleSize, int numberPlate, Color color, String model) {
+        this.vehicleSize = vehicleSize;
+        this.numberPlate = numberPlate;
+        this.color = color;
+        this.model = model;
+        if(this.vehicleSize ==VehicleSize.LARGE){
+            this.slotNeeded=4;
+        }else if(this.vehicleSize==VehicleSize.COMPACT){
+            this.slotNeeded=2;
+        }else{
+            this.slotNeeded=1;
+        }
+    }
+
+    public VehicleSize getVehicleSize() {
+        return vehicleSize;
+    }
+
+    public int getNumberPlate() {
+        return numberPlate;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSlotNeeded() {
+        return slotNeeded;
+    }
 }
